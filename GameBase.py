fc0b904852033402c29235c5fa3dd98406f50348
Josiah Glyshaw <josiah@glyshaw.com>
2024-10-14 21:09:58 -0500

    minor improvements

--- GameBase.py
+++ GameBase.py
@@ -8,53 +8,51 @@ class GameBase:
         self.root = tk.Tk()
         self.root.title("Your Game")
 
-        # Setup the canvas
-        self.canvas = tk.Canvas(self.root)
-        self.canvas.configure(width=rows*size, height=cols*size)
-        self.canvas.pack()
+        # Setup the __canvas
+        self.__canvas = tk.Canvas(self.root, bg='black')
+        self.__canvas.configure(width=rows*size, height=cols*size)
+        self.__canvas.pack()
 
         # Establish game size and grid
         self.rows = rows
         self.cols = cols
-        self.square_size = size
-        self.grid = [[0]*self.cols for i in range(self.rows)]
+        self.__square_size = size
+        self.__grid = [[0]*self.cols for i in range(self.rows)]
 
         # Establish the refresh rate
         self.refresh_rate = refresh_rate
 
         # To hold user input
         self.user_input = 0
-        self.root.bind("<KeyPress>", self.on_keypress)
+        self.root.bind("<KeyPress>", self.__on_keypress)
 
     # Determines if a square already exists at a cordinate
     def square_exists(self, cord):
-        return self.grid[cord[0]][cord[1]] != 0
+        return self.__grid[cord[0]][cord[1]] != 0
 
     # Create a square at a given cordinate.
-    def create_square(self, cord, color):
+    def create_square(self, cord, color, shape = 0):
         if(self.square_exists(cord)):
             raise Exception("ERROR: square there already exists.")
         else:
-            x = cord[0] * self.square_size
-            y = cord[1] * self.square_size
-            self.grid[cord[0]][cord[1]] = self.canvas.create_rectangle(x, y, x + self.square_size, y + self.square_size, fill=color, outline=color)
+            x = cord[0] * self.__square_size
+            y = cord[1] * self.__square_size
+
+            if(shape == "circle"):
+                self.__grid[cord[0]][cord[1]] = self.__canvas.create_oval(x, y, x + self.__square_size, y + self.__square_size, fill=color, outline="black")
+            else:
+                self.__grid[cord[0]][cord[1]] = self.__canvas.create_rectangle(x, y, x + self.__square_size, y + self.__square_size, fill=color, outline="black")
     
     # Delete a square at a given cordinate.
     def delete_square(self, cord):
         if(not self.square_exists(cord)):
             raise Exception("ERROR: no square there exists.")
         else:
-            self.canvas.delete(self.grid[cord[0]][cord[1]]) 
-            self.grid[cord[0]][cord[1]] = 0
+            self.__canvas.delete(self.__grid[cord[0]][cord[1]]) 
+            self.__grid[cord[0]][cord[1]] = 0
 
-    # Store the pressed key into class state.
-    def on_keypress(self, event):
-        self.user_input = event.keysym
-        print(self.user_input)
-
-    # Return last user input.
-    def get_user_input(self):
-        return self.user_input
+    def change_color(self, cord, color):
+        self.__canvas.itemconfigure(self.__grid[cord[0]][cord[1]], fill=color)
 
     # Check if a cordinate is within the grid.
     def inside_grid(self, cord):
@@ -70,3 +68,11 @@ class GameBase:
 
     def game_loop(self):
         raise Exception("This Function is abstract.")
+
+    # Return last user input.
+    def get_user_input(self):
+        return self.user_input
+
+    # Store the pressed key into class state.
+    def __on_keypress(self, event):
+        self.user_input = event.keysym
--- SnakeGame.py
+++ SnakeGame.py
@@ -3,13 +3,13 @@ import random
 
 class Snake(GameBase):
 
-    def __init__(self, rows, cols, size, refresh_rate = 100):
+    def __init__(self, rows, cols, size, refresh_rate = 150):
         super().__init__(rows, cols, size, refresh_rate)
         
         self.apple = []
         self.snake = []
 
-        snake_head = [5,5]
+        snake_head = [int(rows/2), int(cols/2)]
         self.snake.append(snake_head)
         self.create_square(snake_head, "black")
         self.add_apple()
@@ -32,7 +32,7 @@ class Snake(GameBase):
             success = not self.square_exists(apple_cord)
 
         self.apple = apple_cord
-        self.create_square(apple_cord, "red")
+        self.create_square(apple_cord, "red", "circle")
         
 
     def user_action(self):
@@ -50,8 +50,10 @@ class Snake(GameBase):
         elif(user_input == "w"):
             new_head[1] -= 1
 
-        if(self.inside_grid(new_head)):
-   
+        # If ran into self, lost
+        if(self.inside_grid(new_head) and not new_head in self.snake[1:]):
+
+            # If we ate an apple delete it, else delete the tail
             if(new_head == self.apple):
                 self.delete_square(self.apple)
                 self.apple = []
@@ -59,13 +61,18 @@ class Snake(GameBase):
                 self.delete_square(tail)
                 self.snake.pop(-1)
 
-            # Update the head
-            self.create_square(new_head, "green")
+            # Update where the new head is
+            self.create_square(new_head, "white")
             self.snake.insert(0, new_head)
 
+            # Change the second element to a different color
+            if(len(self.snake) > 1):
+                self.change_color(self.snake[1], "gray")
+
+            # If there's no apple make a new one
             if(self.apple == []):
                 self.add_apple()
 
 if __name__ == "__main__":
-    game = Snake(20, 20, 40)
+    game = Snake(20, 15, 40)
     game.start_game()
