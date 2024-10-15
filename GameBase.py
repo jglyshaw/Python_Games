4fa106085fee448c380b25b9ff71eeee5c33da5d
Josiah Glyshaw <32967565+jglyshaw@users.noreply.github.com>
2024-10-15 09:18:35 -0500

    Update GameBase.py

--- GameBase.py
+++ GameBase.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import time
 
 class GameBase:
     def __init__(self, rows, cols, size, refresh_rate = 100):
@@ -16,7 +15,7 @@ class GameBase:
         # Establish game size and grid
         self.rows = rows
         self.cols = cols
-        self.__square_size = size
+        self.__block_size = size
         self.__grid = [[0]*self.cols for i in range(self.rows)]
 
         # Establish the refresh rate
@@ -26,32 +25,33 @@ class GameBase:
         self.user_input = 0
         self.root.bind("<KeyPress>", self.__on_keypress)
 
-    # Determines if a square already exists at a cordinate
-    def square_exists(self, cord):
+    # Determines if a block already exists at a cordinate
+    def block_exists(self, cord):
         return self.__grid[cord[0]][cord[1]] != 0
 
-    # Create a square at a given cordinate.
-    def create_square(self, cord, color, shape = 0):
-        if(self.square_exists(cord)):
-            raise Exception("ERROR: square there already exists.")
+    # Create a block at a given cordinate.
+    def create_block(self, cord, color, shape = 0):
+        if(self.block_exists(cord)):
+            raise Exception("ERROR: block there already exists.")
         else:
-            x = cord[0] * self.__square_size
-            y = cord[1] * self.__square_size
+            x = cord[0] * self.__block_size
+            y = cord[1] * self.__block_size
 
             if(shape == "circle"):
-                self.__grid[cord[0]][cord[1]] = self.__canvas.create_oval(x, y, x + self.__square_size, y + self.__square_size, fill=color, outline="black")
+                self.__grid[cord[0]][cord[1]] = self.__canvas.create_oval(x, y, x + self.__block_size, y + self.__block_size, fill=color, outline="black")
             else:
-                self.__grid[cord[0]][cord[1]] = self.__canvas.create_rectangle(x, y, x + self.__square_size, y + self.__square_size, fill=color, outline="black")
+                self.__grid[cord[0]][cord[1]] = self.__canvas.create_rectangle(x, y, x + self.__block_size, y + self.__block_size, fill=color, outline="black")
     
-    # Delete a square at a given cordinate.
-    def delete_square(self, cord):
-        if(not self.square_exists(cord)):
-            raise Exception("ERROR: no square there exists.")
+    # Delete a block at a given cordinate.
+    def delete_block(self, cord):
+        if(not self.block_exists(cord)):
+            raise Exception("ERROR: no block there exists.")
         else:
             self.__canvas.delete(self.__grid[cord[0]][cord[1]]) 
             self.__grid[cord[0]][cord[1]] = 0
-
-    def change_color(self, cord, color):
+   
+    # Change the color of a existing block.
+    def change_block_color(self, cord, color):
         self.__canvas.itemconfigure(self.__grid[cord[0]][cord[1]], fill=color)
 
     # Check if a cordinate is within the grid.
@@ -63,11 +63,15 @@ class GameBase:
         return True
 
     def start_game(self):
-        self.game_loop()
+        self.run_game()
         self.root.mainloop()
 
+    def run_game(self):
+       self.game_loop()
+       self.root.after(self.refresh_rate, self.run_game)
+        
     def game_loop(self):
-        raise Exception("This Function is abstract.")
+       raise Exception("This Function is abstract.")
 
     # Return last user input.
     def get_user_input(self):
