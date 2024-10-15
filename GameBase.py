5a71aa25401fab0078f576c6dc7240d4ea508fea
Josiah Glyshaw <josiah@glyshaw.com>
2024-10-15 16:52:41 -0500

    small tweaks

--- GameBase.py
+++ GameBase.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 
 class GameBase:
-    def __init__(self, rows, cols, size, refresh_rate = 100):
+    def __init__(self, rows, cols, size, refresh_rate):
         
-        # Set up root
-        self.root = tk.Tk()
-        self.root.title("Your Game")
+        # Set up __root
+        self.__root = tk.Tk()
+        self.__root.title("Your Game")
 
-        # Setup the __canvas
-        self.__canvas = tk.Canvas(self.root, bg='black')
+        # Setup the canvas
+        self.__canvas = tk.Canvas(self.__root, bg='black')
         self.__canvas.configure(width=rows*size, height=cols*size)
         self.__canvas.pack()
 
@@ -19,11 +19,11 @@ class GameBase:
         self.__grid = [[0]*self.cols for i in range(self.rows)]
 
         # Establish the refresh rate
-        self.refresh_rate = refresh_rate
+        self.__refresh_rate = refresh_rate
 
         # To hold user input
-        self.user_input = 0
-        self.root.bind("<KeyPress>", self.__on_keypress)
+        self.__user_input = 0
+        self.__root.bind("<KeyPress>", self.__on_keypress)
 
     # Determines if a block already exists at a cordinate
     def block_exists(self, cord):
@@ -58,21 +58,21 @@ class GameBase:
     def inside_grid(self, cord):
         return 0 <= cord[0] < self.rows and 0 <= cord[1] < self.cols
 
-    def start_game(self):
-        self.run_game()
-        self.root.mainloop()
+    # Return last user input.
+    def get_user_input(self):
+        return self.__user_input
 
-    def run_game(self):
+    def start_game(self):
+        self.__run_game()
+        self.__root.mainloop()
+
+    def __run_game(self):
        self.game_loop()
-       self.root.after(self.refresh_rate, self.run_game)
+       self.__root.after(self.__refresh_rate, self.__run_game)
         
     def game_loop(self):
        raise Exception("This Function is abstract.")
 
-    # Return last user input.
-    def get_user_input(self):
-        return self.user_input
-
     # Store the pressed key into class state.
     def __on_keypress(self, event):
-        self.user_input = event.keysym
+        self.__user_input = event.keysym
--- SnakeGame.py
+++ SnakeGame.py
@@ -7,19 +7,22 @@ class Snake(GameBase):
         super().__init__(rows, cols, size, refresh_rate)
         
         self.apple = []
-        self.snake = []
+        self.add_apple()
 
+        self.snake = []
         snake_head = [rows//2, cols//2]
         self.snake.append(snake_head)
         self.create_block(snake_head, "black")
-        self.add_apple()
 
     def game_loop(self):
+
+        # Update the snake
         self.move_snake()
       
         # If there's no apple make a new one
         if(self.apple == []):
            self.add_apple()
+
 
     def add_apple(self):
         success = False
@@ -48,24 +51,26 @@ class Snake(GameBase):
         elif(user_input == "w"):
             new_head[1] -= 1
 
-        # If ran into self, lost
+        # Prevent movement into self or wall
         if(self.inside_grid(new_head) and not new_head in self.snake[1:]):
 
-            # If we ate an apple delete it, else delete the tail
+            # If we eat an apple delete it, else delete the tail
             if(new_head == self.apple):
                 self.delete_block(self.apple)
                 self.apple = []
             else:
                 self.delete_block(self.snake.pop())
-                
+
             # Update where the new head is
             self.create_block(new_head, "white")
             self.snake.insert(0, new_head)
 
-            # Change the second element to a different color
+            # Change the second segment to a different color
             if(len(self.snake) > 1):
                 self.change_block_color(self.snake[1], "gray")
 
+
+
 if __name__ == "__main__":
-    game = Snake(20, 15, 20)
+    game = Snake(20, 15, 30)
     game.start_game()
