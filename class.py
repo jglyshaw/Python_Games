c8635994ebfee3167a7036d3eae43f3462327ed9
Josiah Glyshaw <josiah@glyshaw.com>
2024-10-14 19:24:48 -0500

    movement

--- class.py
+++ class.py
@@ -2,11 +2,16 @@ import tkinter as tk
 import time
 
 class Game:
-    def __init__(self, rows, cols, size, refresh_rate = 1):
+    def __init__(self, rows, cols, size, refresh_rate = 100):
         
         # Set up root
         self.root = tk.Tk()
         self.root.title("Your Game")
+
+        # Setup the canvas
+        self.canvas = tk.Canvas(self.root)
+        self.canvas.configure(width=len(self.grid[0])*self.square_size, height=len(self.grid)*self.square_size)
+        self.canvas.pack()
 
         # Establish game size and grid
         self.rows = rows
@@ -17,45 +22,45 @@ class Game:
         # Establish the refresh rate
         self.refresh_rate = refresh_rate
 
-        # Setup the canvas
-        self.canvas = tk.Canvas(self.root)
-        self.canvas.configure(width=len(self.grid[0])*self.square_size, height=len(self.grid)*self.square_size)
+        # To hold user input
+        self.user_input = 0
 
-        # Draw the initial grid and pack
-        self.canvas.pack()
+        self.snake = []
 
-        self.key = 0
 
-        self.i = 0
-        self.j = 0
+    # Determines if a square already exists at a cordinate
+    def square_exists(self, cord):
+        return self.grid[cord[0]][cord[1]] != 0
 
-    # Determines if a square already exists at a given row and column.
-    def square_exists(self, row, col):
-        return self.grid[row][col] != 0
-
-    # Create a square at a given row and column.
-    def create_square(self, row, col, color):
-        if(self.square_exists(row,col)):
-            print("error square there already exists")
+    # Create a square at a given cordinate.
+    def create_square(self, cord, color):
+        if(self.square_exists(cord)):
+            raise Exception("error square there already exists")
         else:
-            x = row * self.square_size
-            y = col * self.square_size
-            self.grid[row][col] = self.canvas.create_rectangle(x, y, x + self.square_size, y + self.square_size, fill=color, outline=color)
+            x = cord[0] * self.square_size
+            y = cord[1] * self.square_size
+            self.grid[cord[0]][cord[1]] = self.canvas.create_rectangle(x, y, x + self.square_size, y + self.square_size, fill=color, outline=color)
     
-    # Delete a square at a given row and column.
-    def delete_square(self, row, col):
-        if(not self.square_exists(row,col)):
-            print("error no square there exists")
+    # Delete a square at a given cordinate.
+    def delete_square(self, cord):
+        if(not self.square_exists(cord)):
+            raise Exception("error no square there exists")
         else:
-            self.canvas.delete(self.grid[row][col]) 
-            self.grid[row][col] = 0
+            self.canvas.delete(self.grid[cord[0]][cord[1]]) 
+            self.grid[cord[0]][cord[1]] = 0
 
     # Store the pressed key into class state.
     def on_keypress(self, event):
-        self.key = event.keysym
+        self.user_input = event.keysym
+        print(self.user_input)
 
     def start_game(self):
         self.root.bind("<KeyPress>", self.on_keypress)
+        
+        self.snake.append([5,5])
+        head = self.snake[0]
+        self.create_square(head, "black")
+        
         self.game_loop()
         self.root.mainloop()
     
@@ -66,22 +71,38 @@ class Game:
         # Continue game loop again
         self.root.after(self.refresh_rate, self.game_loop)
 
+    def inside_grid(self, cord):
+        if(cord[0] > self.rows-1 or cord[0] < 0):
+            return False
+        if(cord[1] > self.cols-1 or cord[1] < 0):
+            return False
+        return True
+
     def user_action(self):
-        if(self.key == "space"):
 
-            self.delete_square(self.i,self.j)
+        head = self.snake[0]
+        tail = self.snake[-1]
+        new_head = list(head)
 
-            if(self.i >= self.rows - 1):
-                self.i = 0
-                self.j += 1
-            else:
-                self.i += 1
-            
-            self.create_square(self.i,self.j,"black")
+        if(self.user_input == "d"):
+            new_head[0] += 1
+        elif(self.user_input == "a"):
+            new_head[0] -= 1
+        elif(self.user_input == "s"):
+            new_head[1] += 1
+        elif(self.user_input == "w"):
+            new_head[1] -= 1
 
-            self.key = 0
+        if(self.inside_grid(new_head)):
+            # Delete last segment
+            self.delete_square(tail)
+            self.snake.pop(-1)
+
+            # Add new segment
+            self.create_square(new_head, "black")
+            self.snake.insert(0, new_head)
 
 
 if __name__ == "__main__":
-    game = Game(10, 10, 50)
+    game = Game(20, 20, 50)
     game.start_game()
